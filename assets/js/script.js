7113485c7b2ccf1679799c3fef2c43719e2695c9
Gerald Alarcon <geraldalarcon05@gmail.com>
2018-12-21 04:57:05 -0300

    Rootscope con variable del cookieid

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
 // script.js
-    var demoApp = angular.module('app', ['ngRoute']);
+// Prueba de Admision Desarrollo Web Gerald Alarcón
 
-    demoApp.config(function($routeProvider, $locationProvider) {
+// Router de vistas
+
+    var routerSynergy = angular.module('app', ['ngRoute']);
+
+    routerSynergy.config(function($routeProvider, $locationProvider) {
         $routeProvider
 
             .when('/', {
@@ -17,14 +21,29 @@
             $locationProvider.html5Mode(true);
     });
 
-    demoApp.controller('mainCtrl', function($scope) {
+    routerSynergy.controller('mainCtrl', function($scope) {
         $scope.message = 'Login de SynergyGB';
     });
 
-    demoApp.controller('aboutCtrl', function($scope) {
+    routerSynergy.controller('aboutCtrl', function($scope) {
         $scope.message = 'Panel usado como timeline';
     });
 
+// Definición de models
 
-
+    routerSynergy.controller('appLogin', function($http, $scope, $location, $rootScope) {
+        $scope.user = "";
+        $scope.password = "";
+        $scope.type = "";
+        $scope.submit = function (){
+            $http.post('https://prueba-admision-web.herokuapp.com/session',{"type": $scope.type, "username": $scope.user, "password": $scope.password})
+            .success(function (data) {
+                $scope.events = data;
+                console.log('Este es el cid que esta siendo almacenado: '+data.cid);
+                $rootScope.cookieidentify = data.cid;
+                console.log($rootScope.cookieidentify);
+                $location.path('/timeline');
+            });
+        }
+    });
   
